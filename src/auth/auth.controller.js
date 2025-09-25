d2d6f51a803850655e1ecf106922de3d6470957b
ninily <keidynquinteros@sanjudastadeo.edu.gt>
2025-09-25 15:13:00 -0600

    25/09/2025

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../users/user.model.js'
 import { hash } from 'argon2'
+import { generarJWT } from "../../helpers/JWT-generate.js"
 
 export const register = async ( req, res) => {
     try{
@@ -16,6 +17,13 @@ export const register = async ( req, res) => {
             password: encryptedPassword,
             profilePicture
         })
+        return res.status(200).json({
+            message: "Usuario registrado correctamente",
+            userDetails: {
+                user: newUser.username,
+                email: nuewUser.email,
+            },
+        })
     }catch(error){
         return res.status(500).json({
             message: 'Error al registrar el usuario',
@@ -23,3 +31,42 @@ export const register = async ( req, res) => {
         })
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password, username} = req.body;
+
+    try {
+        const lowerEmail = email ? email.toLowerCase() : null;
+        const lowerUsername = username ? username.toLowerCase() : null;
+
+        const user = await User.findOne({
+            $or: [{ email: lowerEmail }, { username: lowerUsername }],
+        });
+        
+        if (!user) {
+            return res.status(401).json({ message: "Credenciales incorrectas" });
+        }
+
+        const validPassword = await verify(user.password, password);
+        if (!validPassword) {
+            return res.status(401).json({ message: "Credenciales incorrectas" });
+        }
+
+        const token = await generarJWT(user.id, user.email);
+
+        return res.status(200).json({
+            message: "Inicio de sesión exitoso",
+            userDetails: {
+                username: user.username,
+                token: token,
+                profilePicture: user.profilePicture,
+                uid: user.id
+            },
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error del servidor",
+            error: error.message,
+        });
+    }
+};
